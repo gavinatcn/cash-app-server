92218826dc16fe90ff442c7ee81cfb70af8fa4d6
于 卫华 <gavin@KFC-Free-2.local>
2016-12-26 20:59:10 +0800

    增加应用入口及配置文件，需要配置数据库

---
+++ src/main/java/com/yumi/cash/app/server/CashAppServerApplication.java
@@ -0,0 +1,20 @@
+/**
+ * Created by gavin on 2016/12/26.
+ */
+
+package com.yumi.cash.app.server;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("cn.allcheer.acl.app.server.dal*")
+@SpringBootApplication
+public class CashAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashAppServerApplication.class, args);
+    }
+
+}
